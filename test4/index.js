0001ac611ac12507d361a85772b54f6fbc7a31f4
Lipsamayee Jena <91116187+Lipsa760@users.noreply.github.com>
2022-02-06 20:05:42 +0530

    Update index.js

--- test4/index.js
+++ test4/index.js
@@ -2,10 +2,10 @@ let add = document.getElementById('add');
          add.addEventListener('click',function(){
            if(document.getElementById('input_task').value!=""){
 
-             
+            let input1= document.getElementById('input1').value; 
             let input_task= document.getElementById('input_task').value;
             let p = document.createElement('p');
-            p.innerHTML='<span>'+input_task+'</span>'+'<i class="fas fa-edit"></i>'+'<i class="fas fa-trash-alt"></i>'+'</br>'+'</br>'+' <i class="far fa-heart"></i>'+'<i class="far fa-comment"></i>'+'<i class="far fa-share-square"></i>'
+            p.innerHTML='<h4>'+input1+'</h4>'+'<span>'+input_task+'</span>'+'<i class="fas fa-edit"></i>'+'<i class="fas fa-trash-alt"></i>'+'</br>'+'</br>'+' <i class="far fa-heart"></i>'+'<i class="far fa-comment"></i>'+'<i class="far fa-share-square"></i>'
              console.log(input_task);
             console.log(p.children[2]);
             document.getElementById('task').appendChild(p); 
